9dc9fcdda18fe2dd8e3c7bf8fe7404c7083c6b8a
zmrodriguez91 <zmrodriguez91@gmail.com>
2013-04-14 20:11:40 -0700

    mac_conversion -Implemented "-h" switch -Implemented "-D" and "-T" switches

--- Forensics/mac_conversion/main.cpp
+++ Forensics/mac_conversion/main.cpp
@@ -9,8 +9,8 @@ void usage(){
 	return;
 }
 
-long parseHex(string str){
-	long data = 0;
+ int parseHex(string str){
+	int data = 0;
 	if (str[0] != '0') usage();
 	if (str[1] != 'x') usage();
 	for (int i = 2; i < str.length(); i ++)
@@ -18,15 +18,15 @@ long parseHex(string str){
 		data *= 16;
 		if (str[i] >= 'A' && str[i] <= 'F')
 		{
-			data += 10 + (long)(str[i] - 'A');
+			data += 10 + ( int)(str[i] - 'A');
 		}
 		else if (str[i] >='a' && str[i] <= 'f')
 		{
-			data += 10 + (long)(str[i] - 'a');
+			data += 10 + ( int)(str[i] - 'a');
 		}
 		else if (str[i] >='0' && str[i] <= '9')
 		{
-			data += (long)(str[i] - '0');
+			data += ( int)(str[i] - '0');
 		}
 		else usage();
 	}
@@ -63,7 +63,7 @@ int main (int argc, char** argv){
 		}		
 	}
 
-	long data = 0;
+	int data = 0x00000000;
 	if (filename_string != "")
 	{
 
@@ -71,15 +71,68 @@ int main (int argc, char** argv){
 	else if (hex_string != "")
 	{
 		data = parseHex(hex_string);
+		data = ((data & 0xFF00FF00) >> 8)|((data & 0x00FF00FF) << 8);
 	}
-	cout << data << endl;
+	cout << hex << data << dec << endl;
 
 	switch (operation){
 		case Time:
-			cout << "TIME";
+			int Hour, Minute, Second;
+			Hour = (data & 0x0000F800) >> 11;
+			Minute = (data & 0x000007F0) >> 5;
+			Second = (data & 0x0000001F) >> 0;
+			cout << "Time: " << Hour << ":" << Minute << ":" << Second << " ";
+			cout << ((Hour > 12)? "PM" : "AM") << endl;
 			break;
 		case Date:
-			cout << "DATE";
+			int Year, Month, Day;
+			Year = (data & 0x0000FE00) >> 9;
+			Month = (data & 0X000001E0) >> 5;
+			Day = (data & 0x0000001F) >> 0;
+			cout << "Date: ";
+			switch (Month){
+				case 1:
+					cout << "January";
+					break;
+				case 2:
+					cout << "February";
+					break;
+				case 3:
+					cout << "March";
+					break;
+				case 4:
+					cout << "April";
+					break;
+				case 5:
+					cout << "May";
+					break;
+				case 6:
+					cout << "June";
+					break;
+				case 7:
+					cout << "July";
+					break;
+				case 8:
+					cout << "August";
+					break;
+				case 9:
+					cout << "September";
+					break;
+				case 10:
+					cout << "October";
+					break;
+				case 11:
+					cout << "November";
+					break;
+				case 12:
+					cout << "December";
+					break;
+				default:
+					cout << "Invalid Date!" << endl;
+					exit(EXIT_FAILURE);
+					break;
+			}
+			cout << " " << Day << ", " << Year << endl;
 			break;
 		default:
 			usage();
